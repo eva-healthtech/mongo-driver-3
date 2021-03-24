da24a7ff5f555adb439c37ee51b38be052256de8
David Williams <david.williams@evahealth.co.uk>
2021-03-24 08:28:33 +0000

    0.6.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mongo-driver-3 "0.6.1-SNAPSHOT"
+(defproject eva-healthtech/mongo-driver-3 "0.6.1"
   :description "A Clojure wrapper for the Java MongoDB driver 3.11/4.0+."
   :url "https://github.com/gnarroway/mongo-driver-3"
   :license {:name         "The MIT License"
